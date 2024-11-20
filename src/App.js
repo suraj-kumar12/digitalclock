d16ee09b113932f532ffafc4f5d0e15d562aa083
surajkumar12 <surajroman2588@>
2024-11-20 21:04:35 +0530

    increase and decrease

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 
-import React, { useState } from "react";
+
+import React, { useState } from 'react';
 
 const App = () => {
 
-  let time = new Date().toLocaleTimeString();
+  const [update, setUpdate] = useState(0);
+  const increase = (event) => {  
+      setUpdate(event.target.value)
+      setUpdate(update + 1);
+  }
 
-  const[ctime, setCtime] = useState(time); 
-  const updateTime = () => {
-    let time = new Date().toLocaleTimeString();
-    setCtime(time);
-  };
-
-  setInterval(updateTime, 1000);
-
-  return(
+  const decrease = () => {
+    setUpdate(update - 1);
+  }
+    
+  return (
     <>
-        <h1>{time}</h1>
+      <div className="main_div">
+        <div className="center_div">
+          <h1>{update}</h1>
+          <div className="main_btn">
+            <button onClick={increase}>Increase</button>
+            <button onClick={decrease}>Decrease</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
+
 export default App;
